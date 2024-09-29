27568fd0d85f996f275c9a0d80f128e24e9827fe
PhuCuong2512 <PhuCuong2512>
2024-09-29 16:09:55 +0700

    CRUD User

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty({message: "name không được để trống"})
+    name: string;
+
+    @IsNotEmpty({message: "email không được để trống"})
+    @IsEmail({}, {message: "email không đúng định dạng"} )
+    email: string;
+
+    @IsNotEmpty({message: "password không được để trống"})
+    password: string;
+
+    phone: string;
+    address: string;
+    image: string;
+}
